572d504e7526724eaff45d9f6b012730c7e3a618
RunaDh17 <roonadangui@gmail.com>
2025-04-21 18:18:49 +0530

    negative numbers are not allowed and listed
    # 3. Input: “1,-15,20,-6”, Output: negative numbers not allowed -15,-6

--- add.rb
+++ add.rb
@@ -3,6 +3,8 @@
 # 2. Input: “1”, Output: 1
 # 3. Input: “1,15”, Output: 16
 # 3. Input: “1,15,20”, Output: 36
+# 3. Input: “1,-15,20,-6”, Output: negative numbers not allowed -15,-6
+
 
 p "Enter string with numbers  "
 
@@ -11,6 +13,7 @@ num=""
 prev=""
 current=""
 final_numbers=[] # to store all positive numbers in the string
+num_neg=[] # to store all negative numbers in the string
 
 #iteration over the each character of string
 
@@ -19,17 +22,27 @@ num_string.chars.each  do |c|
  if current.match(/\d/)
   if prev.match(/\d/)     # multidigit number is taken care
    num=num+current  #append digits
+  elsif prev== "-"
+      num="-" + current #for multi digit negative number
  else
    num=current # take a new number first digit
  end
  else
-   if num!=""
+   if num!="" and num.to_i > 0
      final_numbers<<  num.to_i
   end
+   if num!="" and num.to_i < 0
+         num_neg<< num.to_i
+      end
    num="" #reset to blank when character is found
  end
   prev=current
 end
-puts  "Sum " + final_numbers.sum.to_s  #ruby >2.4
+
+if num_neg.empty?
+   puts  "Sum " + final_numbers.sum.to_s  #ruby >2.4
+else
+    puts "negative numbers not allowed " + num_neg.join(',')
+end
 
 
